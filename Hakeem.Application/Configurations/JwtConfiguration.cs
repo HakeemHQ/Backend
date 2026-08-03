3b40d9eb36f47cb7bf2c47dc57c843134d60c70a
MazenMustafa1 <mazenmohamed7711@gmail.com>
2026-08-03 17:17:50 +0300

    Increase token limit time for development

--- Hakeem.Application/Configurations/JwtConfiguration.cs
+++ Hakeem.Application/Configurations/JwtConfiguration.cs
@@ -5,6 +5,6 @@ public class JwtConfiguration
     public string Key { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
-    public int TokenExpirationMinutes { get; set; } = 60;
+    public int TokenExpirationMinutes { get; set; } = 120;
     public int RefreshTokenExpirationDays { get; set; } = 7;
 }
